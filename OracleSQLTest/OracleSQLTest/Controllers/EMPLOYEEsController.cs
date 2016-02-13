24ecb1830031582358706cc67a9a28d58ae66aca
anil4321 <anil4321@yahoo.com>
2016-02-12 23:07:49 -0500

    Initial Checkin

---
+++ OracleSQLTest/OracleSQLTest/Controllers/EMPLOYEEsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OracleSQLTest.Models;
+
+namespace OracleSQLTest.Controllers
+{
+    public class EMPLOYEEsController : Controller
+    {
+        private ProfileEntities db = new ProfileEntities();
+
+        // GET: EMPLOYEEs
+        public ActionResult Index()
+        {
+            return View(db.EMPLOYEEs.ToList());
+        }
+
+        // GET: EMPLOYEEs/Details/5
+        public ActionResult Details(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EMPLOYEE eMPLOYEE = db.EMPLOYEEs.Find(id);
+            if (eMPLOYEE == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eMPLOYEE);
+        }
+
+        // GET: EMPLOYEEs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EMPLOYEEs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EMPL_ID,EMPL_TYPE_ID,SERV_CENTER_ID,OFFICE_ID,FCO_DESIGN_CD,EMPL_ZIPCODE,EMPL_FNAME,EMPL_MNAME,EMPL_LNAME,EMPL_SSN,EMPL_TITLE,EMPL_SUPERVISOR,EMPL_WORK_PH_NUM,EMPL_HOME_PH_NUM,EMPL_WORK_FAX_NUM,EMPL_EMAIL_ADDR,EMPL_DOB,EMPL_MOTHER_NM,EMPL_STATUS,USER_ID,CREATED_BY,CREATED_DATE,LAST_UPDATED_BY,LAST_UPDATED_DATE,EMPL_UID")] EMPLOYEE eMPLOYEE)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EMPLOYEEs.Add(eMPLOYEE);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(eMPLOYEE);
+        }
+
+        // GET: EMPLOYEEs/Edit/5
+        public ActionResult Edit(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EMPLOYEE eMPLOYEE = db.EMPLOYEEs.Find(id);
+            if (eMPLOYEE == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eMPLOYEE);
+        }
+
+        // POST: EMPLOYEEs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EMPL_ID,EMPL_TYPE_ID,SERV_CENTER_ID,OFFICE_ID,FCO_DESIGN_CD,EMPL_ZIPCODE,EMPL_FNAME,EMPL_MNAME,EMPL_LNAME,EMPL_SSN,EMPL_TITLE,EMPL_SUPERVISOR,EMPL_WORK_PH_NUM,EMPL_HOME_PH_NUM,EMPL_WORK_FAX_NUM,EMPL_EMAIL_ADDR,EMPL_DOB,EMPL_MOTHER_NM,EMPL_STATUS,USER_ID,CREATED_BY,CREATED_DATE,LAST_UPDATED_BY,LAST_UPDATED_DATE,EMPL_UID")] EMPLOYEE eMPLOYEE)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(eMPLOYEE).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(eMPLOYEE);
+        }
+
+        // GET: EMPLOYEEs/Delete/5
+        public ActionResult Delete(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EMPLOYEE eMPLOYEE = db.EMPLOYEEs.Find(id);
+            if (eMPLOYEE == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eMPLOYEE);
+        }
+
+        // POST: EMPLOYEEs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(decimal id)
+        {
+            EMPLOYEE eMPLOYEE = db.EMPLOYEEs.Find(id);
+            db.EMPLOYEEs.Remove(eMPLOYEE);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
