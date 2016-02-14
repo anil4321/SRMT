09790251e5ea9735ae35665e7f55de2676ffde2e
anil4321 <anil.k.das@uscis.dhs.gov>
2016-02-14 11:42:43 -0500

    Add method to extract Cert Info
    TBD
    1. Make the new function work
    2. Validate the info from the new method

--- OracleSQLTest/OracleSQLTest/Controllers/HomeController.cs
+++ OracleSQLTest/OracleSQLTest/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
 
 namespace OracleSQLTest.Controllers
 {
@@ -28,6 +30,72 @@ namespace OracleSQLTest.Controllers
             ViewBag.crisUser = list;
 
             return View();
+        }
+        public string ReadCert()
+        {
+            X509Certificate2 x509Cert2 = new X509Certificate2(this.Request.ClientCertificate.Certificate);
+            Debug.WriteLine(string.Format("Issued To:"));
+            Debug.WriteLine(string.Format(x509Cert2.Subject));
+
+            Debug.WriteLine(string.Format("Issued By:"));
+            Debug.WriteLine(string.Format(x509Cert2.Issuer));
+
+            Debug.WriteLine(string.Format("Friendly Name:"));
+            Debug.WriteLine(string.Format(string.IsNullOrEmpty(x509Cert2.FriendlyName) ? "(None Specified)" : x509Cert2.FriendlyName));
+
+            Debug.WriteLine(string.Format("Valid Dates:"));
+            Debug.WriteLine(string.Format("From: " + x509Cert2.GetEffectiveDateString()));
+            Debug.WriteLine(string.Format("To: " + x509Cert2.GetExpirationDateString()));
+
+            Debug.WriteLine(string.Format("Thumbprint:"));
+            Debug.WriteLine(string.Format(x509Cert2.Thumbprint));
+
+            //Debug.WriteLine(string.Format( "Public Key:"));
+            //Debug.WriteLine(string.Format( x509Cert2.GetPublicKeyString()));
+
+            #region EKU Section - Retrieve EKU info and write out each OID
+            X509EnhancedKeyUsageExtension ekuExtension = (X509EnhancedKeyUsageExtension)x509Cert2.Extensions["Enhanced Key Usage"];
+            if (ekuExtension != null)
+            {
+                Debug.WriteLine(string.Format("Enhanced Key Usages (" + ekuExtension.EnhancedKeyUsages.Count.ToString() + " found)"));
+
+                OidCollection ekuOids = ekuExtension.EnhancedKeyUsages;
+                foreach (Oid ekuOid in ekuOids)
+                    Debug.WriteLine(string.Format(ekuOid.FriendlyName + " (OID: " + ekuOid.Value + ")"));
+            }
+            else
+            {
+                Debug.WriteLine(string.Format("No EKU Section Data"));
+            }
+            #endregion // EKU Section
+
+            #region Subject Alternative Name Section
+            X509Extension sanExtension = (X509Extension)x509Cert2.Extensions["Subject Alternative Name"];
+            if (sanExtension != null)
+            {
+                Debug.WriteLine(string.Format("Subject Alternative Name:"));
+                Debug.WriteLine(string.Format(sanExtension.Format(true)));
+            }
+            else
+            {
+                Debug.WriteLine(string.Format("No Subject Alternative Name Data"));
+            }
+
+            #endregion // Subject Alternative Name Section
+
+            #region Certificate Policies Section
+            X509Extension policyExtension = (X509Extension)x509Cert2.Extensions["Certificate Policies"];
+            if (policyExtension != null)
+            {
+                Debug.WriteLine(string.Format("Certificate Policies:"));
+                Debug.WriteLine(string.Format(policyExtension.Format(true)));
+            }
+            else
+            {
+                Debug.WriteLine(string.Format("No Certificate Policies Data"));
+            }
+            #endregion //Certificate Policies Section
+            return "uid";
         }
 
         public ActionResult About()
